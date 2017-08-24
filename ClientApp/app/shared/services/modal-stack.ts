ac43406a554e0b60aa029857241becc94b7bf399
Quinntyne Brown <quinntynebrown@gmail.com>
2017-08-24 03:29:54 -0400

    modal

---
+++ ClientApp/app/shared/services/modal-stack.ts
@@ -0,0 +1,32 @@
+﻿//https://github.com/ng-bootstrap/ng-bootstrap/blob/master/src/modal/modal-stack.ts
+
+import { ModalOptions } from "./modal";
+import {
+    ApplicationRef,
+    Injectable,
+    Injector,
+    ReflectiveInjector,
+    ComponentFactory,
+    ComponentFactoryResolver,
+    ComponentRef,
+    TemplateRef
+} from '@angular/core';
+
+import { ModalRef } from "./modal-ref";
+
+@Injectable()
+export class ModalStack {
+    constructor() {
+
+    }
+    
+    public open(
+        componentFactoryResolver: ComponentFactoryResolver,
+        injector: Injector,
+        content: any,
+        modalOptions: ModalOptions
+    ): ModalRef {
+
+        return new ModalRef();
+    }
+}
---
+++ ClientApp/app/shared/services/modal.ts
@@ -0,0 +1,19 @@
+﻿import { Injectable, Injector, ComponentFactoryResolver } from '@angular/core';
+import { ModalRef } from "./modal-ref";
+import { ModalStack } from "./modal-stack";
+
+export interface ModalOptions {
+
+}
+
+export class Modal {
+    constructor(
+        private _componentFactoryResolver: ComponentFactoryResolver,
+        private _injector: Injector,
+        private _modalStack: ModalStack
+    ) { }
+
+    public open(content:any, options: ModalOptions):ModalRef {
+        return this._modalStack.open(this._componentFactoryResolver, this._injector, content, options);
+    }
+}
